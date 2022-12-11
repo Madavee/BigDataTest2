53715ba4d32c4bde31f91c832fd110e4a04c23e2
Madavee <madaveeimesha@gmail.com>
2022-12-11 13:55:40 -0500

    Code Version 7 : Learning rate 1e-5

--- ping.py
+++ ping.py
@@ -9,9 +9,9 @@ time_start = time.process_time()
 
 print ('start time', time.process_time())
 # hyperparameters
-H = 3000 # number of hidden layer neurons
+H = 200 # number of hidden layer neurons
 batch_size = 10 # every how many episodes to do a param update?
-learning_rate = 1e-4
+learning_rate = 1e-5
 gamma = 0.99 # discount factor for reward
 decay_rate = 0.99 # decay factor for RMSProp leaky sum of grad^2
 resume = False # resume from previous checkpoint?
