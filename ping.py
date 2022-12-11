feb0ac84b618da291c093a7cfbbfbaaa4e312624
Madavee <madaveeimesha@gmail.com>
2022-12-11 13:46:01 -0500

    Code Version 5 :Neurons_3200

--- ping.py
+++ ping.py
@@ -9,7 +9,7 @@ time_start = time.process_time()
 
 print ('start time', time.process_time())
 # hyperparameters
-H = 1600 # number of hidden layer neurons
+H = 3200 # number of hidden layer neurons
 batch_size = 10 # every how many episodes to do a param update?
 learning_rate = 1e-4
 gamma = 0.99 # discount factor for reward
